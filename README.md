7895009e9c0151ef3b3c8010d514ba08f5305a50
2rd976w26x-hub <2rd976w26x@privaterelay.appleid.com>
2026-01-07 18:58:26 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Piratwhist – Scorekeeper (v0.1.33)
+# Piratwhist – Scorekeeper (v0.1.34)
 
 ## Rum / multiplayer ✅
 - Opret rum og få en 6-tegns kode
--- piratwhist.js
+++ piratwhist.js
@@ -1,6 +1,6 @@
-/* Piratwhist – v0.1.33 (multiplayer rooms) */
+/* Piratwhist – v0.1.34 (multiplayer rooms) */
 const APP_NAME = "Piratwhist";
-const APP_VERSION = "0.1.33";
+const APP_VERSION = "0.1.34";
 
 const el = (id) => document.getElementById(id);
 
