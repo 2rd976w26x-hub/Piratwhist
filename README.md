0af6cd9e34c778d5ed8dea2bb88e130036b34630
2rd976w26x-hub <2rd976w26x@privaterelay.appleid.com>
2026-01-23 06:42:42 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Piratwhist – Scorekeeper (v0.2.93)
+# Piratwhist – Scorekeeper (v0.2.94)
 
 
 ## Fast layout-regel (masterprompt)
--- piratwhist.js
+++ piratwhist.js
@@ -1,6 +1,6 @@
-/* Piratwhist – v0.2.93 (rooms) */
+/* Piratwhist – v0.2.94 (rooms) */
 const APP_NAME = "Piratwhist";
-const APP_VERSION = "0.2.93";
+const APP_VERSION = "0.2.94";
 
 const el = (id) => document.getElementById(id);
 
